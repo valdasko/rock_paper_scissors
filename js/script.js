734672597688d83c886a356e76efe429caff13f2
Valdas <valdaskontenis1@gmail.com.com>
2023-02-16 17:11:20 +0200

    dom

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
 'use strict';
 console.log('script.js');
 
-let playerScore = 0;
-let computerScore = 0;
-let round = 0;
+const roundResultEl = document.getElementById('roundResult');
+const playerText = document.getElementById('playersPick');
+const computerText = document.getElementById('computersPick');
+const playerResult = document.getElementById('playerResult');
+const computerResult = document.getElementById('computerResult');
+
+let playerScore = '0';
+let computerScore = '0';
+let round = '0';
+let computersChoice;
 
 // Computer chooses 1 random option
 function getComputersChoice() {
   let computerOptions = ['rock', 'paper', 'scissors'];
-  let computersChoice =
+  computersChoice =
     computerOptions[Math.floor(Math.random() * computerOptions.length)];
-  console.log('Computer picked', computersChoice);
   return computersChoice;
 }
 
-// Plays one round
 function playOneRound(playersChoice) {
-  if (
-    playersChoice !== 'rock' &&
-    playersChoice !== 'paper' &&
-    playersChoice !== 'scissors'
-  ) {
-    console.log('wrong input');
-    return;
-  }
-  let computersChoice = getComputersChoice();
+  computersChoice = getComputersChoice();
+  computerText.textContent = computersChoice;
   if (playersChoice === computersChoice) {
-    console.log('DRAW !');
+    roundResultEl.textContent = 'Draw';
     return;
   } else if (
     (playersChoice === 'rock' && computersChoice === 'scissors') ||
@@ -34,38 +32,34 @@ function playOneRound(playersChoice) {
     (playersChoice === 'scissors' && computersChoice === 'paper')
   ) {
     let playerWin = 'You WIN';
-    console.log('YOU WIN!');
+    roundResultEl.textContent = 'Player Win';
     playerScore++;
+    playerResult.textContent = playerScore.toString();
     return playerWin;
   } else {
-    let computerWin = 'You lost';
-    console.log('YOU LOST!');
+    let computerWin = 'Computer WIN';
+    roundResultEl.textContent = 'You lost. Computer win';
     computerScore++;
+    computerResult.textContent = computerScore.toString();
     return computerWin;
-    // return computerWin;
   }
 }
-function game() {
-  for (let i = 0; playerScore < 5 && computerScore < 5; i++) {
-    playOneRound();
-    console.log(`Players score: ${playerScore}`);
-    console.log(`Computers score: ${computerScore}`);
-  }
-}
-// game();
 
 let rockBtn = document.getElementById('rockBtn');
 let paperBtn = document.getElementById('paperBtn');
 let scissorsBtn = document.getElementById('scissorsBtn');
 
-// rockBtn.addEventListener('click', () => {
-//   playOneRound('rock');
-// });
+rockBtn.addEventListener('click', () => {
+  playerText.textContent = 'Rock';
+  playOneRound('rock');
+});
 
-// paperBtn.addEventListener('click', () => {
-//   playOneRound('paper');
-// });
+paperBtn.addEventListener('click', () => {
+  playerText.textContent = 'Paper';
+  playOneRound('paper');
+});
 
-// scissorsBtn.addEventListener('click', () => {
-//   playOneRound('scissors');
-// });
+scissorsBtn.addEventListener('click', () => {
+  playerText.textContent = 'Scissors';
+  playOneRound('scissors');
+});
